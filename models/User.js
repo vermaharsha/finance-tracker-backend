dbe907ce3b18ef49c2267275feeac16cea9ecb0b
Harsha Verma <vermaharsha939@gmail.com>
2025-07-06 20:10:40 +0530

    feat: add Transaction and User models with schema definitions

--- models/User.js
+++ models/User.js
@@ -1,1 +1,21 @@
 // models/User.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /.+\@.+\..+/,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+});
+
+model.exports = mongoose.model('User', userSchema);
